521abc695f82093e0a5fde20e7a802ab665f1ca5
z9877969 <z9877969@ukr.net>
2026-03-06 17:20:53 +0200

    adds logout user

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 import { registerUser } from './auth/registerUser.js';
+import { logoutUser } from './auth/logoutUser.js';
 
 import { getCategories } from './categories/getCategories.js';
 
 export const auth = {
   registerUser,
+  logoutUser,
 };
 
 export const categories = {
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -5,5 +5,6 @@ import { auth as ctrl } from '../controllers/index.js';
 const authRouter = Router();
 
 authRouter.post('/register', ctrl.registerUser);
+authRouter.post('/logout', ctrl.logoutUser);
 
 export default authRouter;
